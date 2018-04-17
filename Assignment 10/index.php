5d32c6fd45c8ac1c0a1e86c942ab90a54beeadf2
Henry Fritz <henryamsterfritz@gmail.com>
2018-04-17 00:49:32 -0400

    fixed previous bad commit

--- Assignment 10/index.php
+++ Assignment 10/index.php
@@ -1,11 +1,10 @@
-<<<<<<< HEAD
 <html><head><title>PHP Assignment 1</title></head>
 <body>
 <?php
     $name = 'Henry';
     $string1 = "Welcome to PHP Basics".;
     $string2 = "My name is" . $name . ". ";
-    $string3 = "Tomorrow" . $name . " will learn basic PHP"; 
+    $string3 = "Tomorrow Henry will learn basic PHP"; 
 ?>
     <h1><?= $string1 ?></$string1></h1>
     <h2><?= $string2 ?></$string1></h2>
@@ -15,21 +14,3 @@
 </html>
 
 
-=======
-<html><head><title>PHP Assignment 1</title></head>
-<body>
-<?php
-    $name = 'Henry';
-    $string1 = "Welcome to PHP Basics".;
-    $string2 = "My name is" . $name . ". ";
-    $string3 = "Tomorrow" . $name . " will learn basic PHP"; 
-?>
-    <h1><?= $string1 ?></$string1></h1>
-    <h2><?= $string2 ?></$string1></h2>
-    <h3><?= $string3 ?></$string1></h3>
-</body>
-
-</html>
-
-
->>>>>>> 2faf6e7e5e6514edb8dac68102161781c2c1f2fd
