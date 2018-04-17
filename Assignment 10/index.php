7023ad7c8d5894f9dae796834901437e2e88eddd
Henry Fritz <henryamsterfritz@gmail.com>
2018-04-16 21:23:00 -0400

    added two PHP assignments- 10 & 11

---
+++ Assignment 10/index.php
@@ -0,0 +1,16 @@
+<html><head><title>PHP Assignment 1</title></head>
+<body>
+<?php
+    $name = 'Henry';
+    $string1 = "Welcome to PHP Basics".;
+    $string2 = "My name is" . $name . ". ";
+    $string3 = "Tomorrow Henry will learn basic PHP"; 
+?>
+    <h1><?= $string1 ?></$string1></h1>
+    <h2><?= $string2 ?></$string1></h2>
+    <h3><?= $string3 ?></$string1></h3>
+</body>
+
+</html>
+
+
